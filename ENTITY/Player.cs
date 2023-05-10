02203267717aafb2b1bd2d9bb5b3bc9f454102b6
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-05-10 14:02:14 +0200

    Add project files.

---
+++ ENTITY/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY
+{
+    public class Player
+    {
+
+        [Key]
+        public int PlayerID { get; set; }
+
+
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(50)]
+        public string Surname { get; set; }
+        [MaxLength (30)]
+        public int Score { get; set; }
+        [MaxLength (50)]
+        public int  GamesWon{ get; set; }
+        [MaxLength(50)]
+        public int  GamesPlayed { get; set; }
+       
+
+
+    }
+}
---
+++ ENTITY/PlayerContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY
+{
+    public  class PlayerContext:DbContext
+    {
+        public DbSet<Player> Player { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=EfPlayer;" +
+                "Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
+

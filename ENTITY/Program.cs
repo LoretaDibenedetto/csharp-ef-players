7ad8eca36606ac1ff5b5817bbc79d0b720f01bd6
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-05-10 15:43:30 +0200

    add modifire name and surname player

--- ENTITY/Program.cs
+++ ENTITY/Program.cs
@@ -19,7 +19,7 @@ while (continua)
     Console.WriteLine("1. Inserisci un player");
 
     Console.WriteLine("2. Ricerca un player per id");
-
+    Console.WriteLine("3. Modifica nome giocatore per id");
     Console.WriteLine("5. Cancella un player");
     Console.WriteLine("6. Esci");
 
@@ -50,6 +50,7 @@ while (continua)
             {
                 db.Add(player1);
                 db.SaveChanges();
+                Console.WriteLine(player1.ToString());
             }
             break;
         case 2:
@@ -67,22 +68,48 @@ while (continua)
             }
 
 
+            break;
 
+            case 3:
+            Console.WriteLine("inserisci l'id del giocatore da eliminare");
+            int idFoundPlayers2 = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("inserisci il nuovo nome:");
+            string newName = Console.ReadLine();
 
+            Console.WriteLine("inserisci il nuovo cognome:");
+            string newSurname = Console.ReadLine();
 
+            using (PlayerContext db = new PlayerContext())
+            {
+                Player playersFound = db.Player.Where(playerScansionato => playerScansionato.PlayerID == idFoundPlayers2).First();
+                Console.WriteLine("giocatore prima della modifica:  " + playersFound.ToString());
+
+                playersFound.Name = newName;
+                playersFound.Surname = newSurname;
+
+                Console.WriteLine("giocatore dopo la modifica: " + playersFound.ToString());
+
+                Console.WriteLine();
+
+                db.SaveChanges();
+            }
 
             break;
 
         case 5:
             continua = false;
             break;
+
+        default:
+            Console.WriteLine("Non hai inserito un'opzione valida! Ritenta!");
+            break;
     }
 
 
     //mi ha obbligata ad aggiungerla :(
     static void main(string[] args)
-{
+{       
 
   
     }
